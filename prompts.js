bfba179521f728c88b8292bfc958dbd77adc2666
Sebastian Fitzner <sebastian.fitzner@aperto.com>
2018-03-06 17:12:25 +0100

    Add array and entity selection

--- prompts.js
+++ prompts.js
@@ -1,13 +1,6 @@
 module.exports = (context) => {
 	return [
 		{
-			when: (answers) => answers.blueprints.indexOf('/store/bp.reducer.js.ejs') !== -1,
-			type: 'confirm',
-			name: 'entitiesObject',
-			message: 'Do you want to save your entities payload in an object based structure?',
-			default: true
-		},
-		{
 			when: (answers) => answers.blueprints.indexOf('/services/bp.service.js.ejs') !== -1,
 			type: 'confirm',
 			name: 'service',
